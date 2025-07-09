559906acbd6c403a7d5e4644841b413faa38a635
Gahnavi133 <159682846+Gahnavi133@users.noreply.github.com>
2025-07-09 08:43:54 +0530

    Update code_runner.py

--- code_runner.py
+++ code_runner.py
@@ -2,6 +2,6 @@ def safe_exec(code):
     try:
         exec_globals = {}
         exec(code, exec_globals)
-        return "✅ Code ran successfully", ""
+        return "Code ran successfully", ""
     except Exception as e:
-        return "", f"❌ Error: {e}"
+        return "", f" Error: {e}"
